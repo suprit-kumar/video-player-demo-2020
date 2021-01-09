4939725d917351f42804f643d8a77f4469092719
Supritkumar <supritk16@gmail.com>
2021-01-09 20:34:18 +0530

    populate duration and current time

--- script.js
+++ script.js
@@ -36,9 +36,21 @@ video.addEventListener('ended',showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(time){
+    const minutes = Math.floor(time/60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`;
+    return `${minutes}:${seconds}`;
+}
+
+
+
 // Update progress bar as the video plays
 function updateProgress(){
     progressBar.style.width = `${(video.currentTime/video.duration)*100}%`;
+    currentTime.textContent = `${displayTime(video.currentTime)} /`;
+    duration.textContent = `${displayTime(video.duration)}`;
 }
 
 
